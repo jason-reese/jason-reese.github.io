d452e9aa06c55b75e46f3518304aabd5e2991910
jason-reese <58480450+jason-reese@users.noreply.github.com>
2024-10-04 23:08:11 -0600

    again

--- leaflet/weather/weather.js
+++ leaflet/weather/weather.js
@@ -1,6 +1,6 @@
 var map = L.map('weathermap').setView([38, -95], 4);
-var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var basemap = L.tileLayer(basemapUrl, {attribution: '&copy; <a href = "http://' + 'www.openstreetmap.org/copyright">OpenStreetMap</a>'})
+var basemapUrl = 'https://.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var basemap = L.tileLayer(basemapUrl, {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'})
 
 var radarUrl ='https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
 var radarDisplayOptions = {
