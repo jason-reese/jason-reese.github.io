1834b60415a3ea2381b2f6a982c245f9bc28e007
jason-reese <58480450+jason-reese@users.noreply.github.com>
2024-10-04 22:50:07 -0600

    test

--- leaflet/weather/weather.js
+++ leaflet/weather/weather.js
@@ -19,7 +19,7 @@ $.getJSON(weatherAlertsUrl, function(data) {
       return { color: alertColor};
     }
       onEachFeature: function(feature, layer){
-        layer.bindPopup(feature.properties.heading)
+        layer.bindPopup(feature.properties.headline)
       }
           
   }).addTo(map);
