aa9e8b6e1565b86a655c0d3de4a76bd78be5d130
jason-reese <58480450+jason-reese@users.noreply.github.com>
2024-10-04 23:22:18 -0600

    test

--- leaflet/weather/weather.js
+++ leaflet/weather/weather.js
@@ -1,9 +1,9 @@
 var map = L.map('weathermap').setView([38, -95], 4);
-var basemapUrl = 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}';
-var basemap = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
-});
+var basemapUrl = 'ttps://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}';
+var basemap = L.tileLayer(basemapUrl)
+
+
+
 var radarUrl ='https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
 var radarDisplayOptions = {
   layers: 'nexrad-n0r-900913',
