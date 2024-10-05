f6d71b9c20e35a7df11b76cae3d8b692dc1d44a2
jason-reese <58480450+jason-reese@users.noreply.github.com>
2024-10-04 22:45:32 -0600

    this time

--- leaflet/weather/weather.js
+++ leaflet/weather/weather.js
@@ -2,7 +2,7 @@ var map = L.map('weathermap').setView([38, -95], 4);
 var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var basemap = L.tileLayer(basemapUrl, {attribution: '&copy; <a href = "http://' + 'www.openstreetmap.org/copyright">OpenStreetMap</a>'})
 
-var radarUrl ='https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
+var radarUrl ='https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi?';
 var radarDisplayOptions = {
   layers: 'nexrad-n0r-900913',
   format: 'image/png',
@@ -18,5 +18,9 @@ $.getJSON(weatherAlertsUrl, function(data) {
       if (feature.properties.severity === 'Severe') alertColor = 'red';
       return { color: alertColor};
     }
+      onEachFeature: function(feature, layer){
+        layer.bindPopup(feature.properties.heading)
+      }
+          
   }).addTo(map);
 });
