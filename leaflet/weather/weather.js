fab36cf31f2ba673b3d4d39465c421d57df03597
jason-reese <58480450+jason-reese@users.noreply.github.com>
2024-10-04 23:25:42 -0600

    test

--- leaflet/weather/weather.js
+++ leaflet/weather/weather.js
@@ -1,5 +1,5 @@
 var map = L.map('weathermap').setView([38, -95], 4);
-var basemapUrl = 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}';
+var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var basemap = L.tileLayer(basemapUrl)
 
 
