b8f71c7532fb356d64f688fd18381e63c5a94c61
jason-reese <58480450+jason-reese@users.noreply.github.com>
2024-10-06 22:01:05 -0600

    list

--- ArcGIS/obj.js
+++ ArcGIS/obj.js
@@ -2,11 +2,11 @@ var myStuff = {
     "Graduate School": { city: "Laramie", state: "Wyoming", "coord": [-105.5911, 41.3115] },
     "Undergrade School": { city: "Bozeman", state: "Montana", "coord": [-111.0429, 45.6760 ] },
     "Current Residence": { city: "Kuna", state: "Idaho", "coord": [-116.3882, 43.5787 ] },
-    "Visited Ski Resort": { city: "Whistler-Blackcomb", state: "British Columbia", "coord": [-117.161087, 32.715736] },
-    "Visited Ski Resort": { city: "Sun Valley Resort", state: "Idaho", "coord": [-114.3540, 43.6850] },
-    "Visited Ski Resort": { city: "Schweitzer Mountain Resort", state: "Idaho", "coord": [-116.5751, 48.3185] },
-    "Visited Ski Resort": { city: "Brundage Mountain Resort", state: "Idaho", "coord": [-115.8530, 44.7924] },
-    "Visited Ski Resort": { city: "Silver Mountain Resort", state: "Idaho", "coord": [-116.1586, 47.3504] },
-    "Visited Ski Resort": { city: "Pomerelle Mountain Resort", state: "Idaho", "coord": [-113.5124, 42.5060] },
-    "Visited Ski Resort": { city: "Tamarack Resort", state: "Idaho", "coord": [-116.1195, 44.3995] }
+    "Visited Ski Resort 1": { city: "Whistler-Blackcomb", state: "British Columbia", "coord": [-117.1610, 32.7157] },
+    "Visited Ski Resort 2": { city: "Sun Valley Resort", state: "Idaho", "coord": [-114.3540, 43.6850] },
+    "Visited Ski Resort 3 ": { city: "Schweitzer Mountain Resort", state: "Idaho", "coord": [-116.5751, 48.3185] },
+    "Visited Ski Resort 4": { city: "Brundage Mountain Resort", state: "Idaho", "coord": [-115.8530, 44.7924] },
+    "Visited Ski Resort 5": { city: "Silver Mountain Resort", state: "Idaho", "coord": [-116.1586, 47.3504] },
+    "Visited Ski Resort 6": { city: "Pomerelle Mountain Resort", state: "Idaho", "coord": [-113.5124, 42.5060] },
+    "Visited Ski Resort 7 ": { city: "Tamarack Resort", state: "Idaho", "coord": [-116.1195, 44.3995] }
 }
