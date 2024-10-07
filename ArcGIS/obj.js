7b1926cd32f0cb619b8d8686cd33d33c7e7b9af5
jason-reese <58480450+jason-reese@users.noreply.github.com>
2024-10-06 21:55:28 -0600

    list

--- ArcGIS/obj.js
+++ ArcGIS/obj.js
@@ -1,7 +1,7 @@
 var myStuff = {
-    "Graduate School": { city: "Laramie", state: "Wyoming", "coord": [-147.776749, 64.835365] },
-    "Undergrade School": { city: "Bozeman", state: "Montana", "coord": [-80.191788, 25.761681] },
-    "Current Residence": { city: "Kuna", state: "Idaho", "coord": [-105.502, 41.3104] },
+    "Graduate School": { city: "Laramie", state: "Wyoming", "coord": [-105.5911, 41.3115] },
+    "Undergrade School": { city: "Bozeman", state: "Montana", "coord": [-111.0429, 45.6760 ] },
+    "Current Residence": { city: "Kuna", state: "Idaho", "coord": [-116.3882, 43.5787 ] },
     "Visited Ski Resort": { city: "Whistler-Blackcomb", state: "British Columbia", "coord": [-117.161087, 32.715736] },
     "Visited Ski Resort": { city: "Sun Valley Resort", state: "Idaho", "coord": [-114.3540, 43.6850] },
     "Visited Ski Resort": { city: "Schweitzer Mountain Resort", state: "Idaho", "coord": [-116.5751, 48.3185] },
