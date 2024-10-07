0dab1a00c630778af70e603b917cf8bed3d8c785
jason-reese <58480450+jason-reese@users.noreply.github.com>
2024-10-06 21:52:19 -0600

    list

--- ArcGIS/obj.js
+++ ArcGIS/obj.js
@@ -2,11 +2,11 @@ var myStuff = {
     "Graduate School": { city: "Laramie", state: "Wyoming", "coord": [-147.776749, 64.835365] },
     "Undergrade School": { city: "Bozeman", state: "Montana", "coord": [-80.191788, 25.761681] },
     "Current Residence": { city: "Kuna", state: "Idaho", "coord": [-105.502, 41.3104] },
-    "Visited Ski Resort": { city: "Whistler-Blackcomb", state: "British Columbia", "coord": [-117.161087, 32.715736] }
-    "Visited Ski Resort": { city: "Sun Valley Resort", state: "Idaho", "coord": [-114.3540, 43.6850] }         
-    "Visited Ski Resort": { city: "Schweitzer Mountain Resort", state: "Idaho", "coord": [-116.5751, 48.3185] }
-    "Visited Ski Resort": { city: "Brundage Mountain Resort", state: "Idaho", "coord": [-115.8530, 44.7924] }
-    "Visited Ski Resort": { city: "Silver Mountain Resort", state: "Idaho", "coord": [-116.1586, 47.3504] }
-    "Visited Ski Resort": { city: "Pomerelle Mountain Resort", state: "Idaho", "coord": [-113.5124, 42.5060] }
+    "Visited Ski Resort": { city: "Whistler-Blackcomb", state: "British Columbia", "coord": [-117.161087, 32.715736] },
+    "Visited Ski Resort": { city: "Sun Valley Resort", state: "Idaho", "coord": [-114.3540, 43.6850] },
+    "Visited Ski Resort": { city: "Schweitzer Mountain Resort", state: "Idaho", "coord": [-116.5751, 48.3185] },
+    "Visited Ski Resort": { city: "Brundage Mountain Resort", state: "Idaho", "coord": [-115.8530, 44.7924] },
+    "Visited Ski Resort": { city: "Silver Mountain Resort", state: "Idaho", "coord": [-116.1586, 47.3504] },
+    "Visited Ski Resort": { city: "Pomerelle Mountain Resort", state: "Idaho", "coord": [-113.5124, 42.5060] },
     "Visited Ski Resort": { city: "Tamarack Resort", state: "Idaho", "coord": [-116.1195, 44.3995] }
 }
